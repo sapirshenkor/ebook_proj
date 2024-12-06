9c56358ee0d610b1653a597f29fe222d2e230510
hliran2 <hliran2@gmail.com>
2024-12-06 17:51:06 +0200

    add user page

---
+++ EBook_Proj/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using EBook_Proj.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EBook_Proj.Controllers;
+
+public class UserController : Controller
+{
+    // GET
+    public ActionResult Index()
+    {
+        return View();
+    }
+
+    public ActionResult UserPage(UserModel user)
+    {
+        
+      //check validation
+      if (ModelState.IsValid)
+      {
+          return View("UserPageView",user);
+      }
+      
+      return View("CreateUserView", user);
+    }
+
+    public ActionResult CreateUser()
+    {
+        return View("CreateUserView", new UserModel());
+    }
+    
+    
+    
+}
---
+++ EBook_Proj/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EBook_Proj.Models;
+
+public class UserModel
+{
+    [Required(ErrorMessage = "Username is required")]
+    public string Username { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "First name is required")]
+    public string Name { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Last name is required")]
+    public string LastName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid email address")]
+    public string Email { get; set; } = string.Empty;
+}
