47f228d0a40dc8bbdb8d77a599d7ba06bb8aac9e
Hliran207 <liranho@ac.sce.ac.il>
2024-12-06 13:01:00 +0200

    testing

--- EBook_Proj/Controllers/HomeController.cs
+++ EBook_Proj/Controllers/HomeController.cs
@@ -29,8 +29,9 @@ public class HomeController : Controller
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 
-    public IActionResult index()
+    public IActionResult index2()
     {
+        // testing
         return View();
     }
 }
