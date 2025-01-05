e2eb95795fbbea38de334fa76de7942f9520067e
sapirshenkor <sapir000311@gmail.com>
2025-01-05 13:02:12 +0200

    edit books format for download

--- EBook_Proj/Controllers/BooksController.cs
+++ EBook_Proj/Controllers/BooksController.cs
@@ -86,12 +86,48 @@ public class BooksController: Controller
 
     public async Task<IActionResult> BookDetails(int id)
     {
+        
         var book= await _context.Books.FirstOrDefaultAsync(b=>b.BookID == id);
         if (book == null)
         {
             return NotFound();
         }
         return View(book);
+    }
+    [HttpGet]
+    public IActionResult Download(string format)
+    {
+        byte[] fileBytes;
+        string contentType;
+        string fileName;
+
+        // Create empty file bytes - just a small placeholder
+        fileBytes = new byte[] { 0x0 };
+
+        switch (format.ToLower())
+        {
+            case "epub":
+                contentType = "application/epub+zip";
+                fileName = $"book.epub";
+                break;
+            case "fb2":
+                contentType = "application/xml";
+                fileName = $"book.fb2";
+                break;
+            case "mobi":
+                contentType = "application/x-mobipocket-ebook";
+                fileName = $"book.mobi";
+                break;
+            case "pdf":
+                contentType = "application/pdf";
+                fileName = $"book.pdf";
+                break;
+            default:
+                return BadRequest("Unsupported format");
+        }
+
+        return File(fileBytes, contentType, fileName);
+    
     }
 
     
