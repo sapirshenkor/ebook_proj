644800881132b6eae185d7f3eee2af13e8d1a566
sapirsh5 <sapirsh5@ac.sce.ac.il>
2025-05-04 15:26:22 +0300

    pushing again

---
+++ EBook_Proj/Models/PasswordHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+public static class PasswordHelper
+{
+    public static string HashWithSha256(string input)
+    {
+        using (var sha256 = SHA256.Create())
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(input);
+            byte[] hashBytes = sha256.ComputeHash(bytes);
+            return Convert.ToHexString(hashBytes); // since .NET 5; if using older version use BitConverter.ToString().Replace("-", "")
+        }
+    }
+}
---
+++ EBook_Proj/Models/ResetPasswordViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class ResetPasswordViewModel
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    public string TemporaryPassword { get; set; }
+
+    [Required(ErrorMessage = "Please Enter Password")]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$", 
+        ErrorMessage = "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character.")]
+    [StringLength(100)]
+    public string NewPassword { get; set; }
+
+    [Required]
+    [Compare("NewPassword", ErrorMessage = "Passwords do not match.")]
+    [DataType(DataType.Password)]
+    public string ConfirmNewPassword { get; set; }
+}
